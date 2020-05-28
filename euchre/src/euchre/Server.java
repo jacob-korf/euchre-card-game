63dee53de8a0312b220c103992f11af0101cadce
Jacob Korf <korfj7072@uwec.edu>
2020-05-27 19:47:17 -0500

    Limit connections to 4 connections to server

--- euchre/src/euchre/Server.java
+++ euchre/src/euchre/Server.java
@@ -36,39 +36,42 @@ public class Server
 			//		(any code following the accept call will execute after the accept() succeeds) or failed due to the timeout (throws a 
 			//		SocketTimeoutException).  See the Java docs for the ServerSocket class or use online search on this topioc for details.
 			// Wait for and accept an incoming request
-			s = ss.accept(); 
-			// System.out.println("New client request received : " + s); 
-         
-			// create input and output streams for this socket 
-			DataInputStream dis = new DataInputStream(s.getInputStream()); 
-			DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
+			if(ar.size() < 4) {
 
-			// Create a new handler object for handling this request.
-			String clientName = "client " + i;
-			ClientHandler mtch = new ClientHandler(s, clientName, dis, dos); 
+				s = ss.accept(); 
+				// System.out.println("New client request received : " + s); 
+	         
+				// create input and output streams for this socket 
+				DataInputStream dis = new DataInputStream(s.getInputStream()); 
+				DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
 
-			// Create a new Thread with this client handler object. 
-			Thread t = new Thread(mtch); 
-           
-			System.out.println("Adding client " + clientName  + " to active client list"); 
+				// Create a new handler object for handling this request.
+				String clientName = "client " + i;
+				ClientHandler mtch = new ClientHandler(s, clientName, dis, dos); 
 
-			// add this client to active clients list 
-			ar.add(mtch); 
-			
-			// display client list
-			System.out.println("Current Clients:");
-			for (ClientHandler mc : Server.ar) { 
-				if (mc.isloggedin == true) {
-					System.out.println(mc.name);
-				}
-			} 
-			System.out.println();
-			
-			// start the thread. 
-			t.start(); 
+				// Create a new Thread with this client handler object. 
+				Thread t = new Thread(mtch); 
+	           
+				System.out.println("Adding client " + clientName  + " to active client list"); 
 
-			// increment i for new client name
-			i++; 
+				// add this client to active clients list 
+				ar.add(mtch); 
+				
+				// display client list
+				System.out.println("Current Clients:");
+				for (ClientHandler mc : Server.ar) { 
+					if (mc.isloggedin == true) {
+						System.out.println(mc.name);
+					}
+				} 
+				System.out.println();
+				
+				// start the thread. 
+				t.start(); 
+
+				// increment i for new client name
+				i++; 
+			}
 
 		}	// end - while true loop 
 	}	// end - method main
