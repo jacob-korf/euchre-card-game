04325a6eeff3afa049c4920bd1f1a8d6272815fc
Jacob Korf <korfj7072@uwec.edu>
2020-06-01 16:18:01 -0500

    Designed Hub to have correct buttons

--- euchre/src/euchre/Hub.java
+++ euchre/src/euchre/Hub.java
@@ -22,16 +22,9 @@ import javax.swing.WindowConstants;
 
 public class Hub extends JFrame{
 		private String[] string = {"Hello", "Goodbye"};
-		private static JLabel currentClients = new JLabel("Current Client: ");
-		private JLabel workstation = new JLabel("WorkStation             ");
-		private JLabel RequestTime = new JLabel("   RequestTime          ");
-		private JLabel WaitTime = new JLabel("   WaitTime");
-		private JLabel positionNumber = new JLabel("PositionNumber     ");
-		private JLabel preferName = new JLabel("Name             ");
-		private static JLabel clock = new JLabel();
-		private static JTextArea queue = null;
 		private static Boolean connect = false;
 		private JButton connectToGame = new JButton("Connect to this Game");
+		private JButton createGame = new JButton("Create New Game");
 		private JComboBox<String> gameOptionsBox = new JComboBox<String>(string);
 
 		public static void main(String[] args) {
@@ -86,63 +79,28 @@ public class Hub extends JFrame{
 			// Adding all display components to the gui
 			constraints.gridx = 0;
 			constraints.gridy = 0;
-			constraints.gridwidth = 2;
+			constraints.gridwidth = 1;
 			constraints.gridheight = 1;
 			constraints.anchor = GridBagConstraints.WEST;
 			newPanel.add(gameOptionsBox, constraints);
+			
+			constraints.gridx = 1;
+			constraints.gridy = 0;
+			constraints.gridwidth = 1;
+			constraints.gridheight = 1;
+			constraints.anchor = GridBagConstraints.WEST;
+			newPanel.add(connectToGame, constraints);
+			
 
 			constraints.gridx = 0;
 			constraints.gridy = 1;
-			constraints.gridwidth = 4;
+			constraints.gridwidth = 2;
 			constraints.gridheight = 1;
 			constraints.anchor = GridBagConstraints.WEST;
-			newPanel.add(currentClients, constraints);
+			newPanel.add(createGame, constraints);
 
-			constraints.gridx = 0;
-			constraints.gridy = 2;
-			constraints.gridwidth = 1;
-			constraints.gridheight = 1;
-			constraints.anchor = GridBagConstraints.WEST;
-			newPanel.add(positionNumber, constraints);
 
-			constraints.gridx = 1;
-			constraints.gridy = 2;
-			constraints.gridwidth = 1;
-			constraints.gridheight = 1;
-			constraints.anchor = GridBagConstraints.WEST;
-			newPanel.add(workstation, constraints);
 
-			constraints.gridx = 2;
-			constraints.gridy = 2;
-			constraints.gridwidth = 1;
-			constraints.gridheight = 1;
-			constraints.anchor = GridBagConstraints.WEST;
-			newPanel.add(preferName, constraints);
-			
-			constraints.gridx = 3;
-			constraints.gridy = 2;
-			constraints.gridwidth = 1;
-			constraints.gridheight = 1;
-			constraints.anchor = GridBagConstraints.WEST;
-			newPanel.add(RequestTime, constraints);
-
-			constraints.gridx = 4;
-			constraints.gridy = 2;
-			constraints.gridwidth = 1;
-			constraints.gridheight = 1;
-			constraints.anchor = GridBagConstraints.WEST;
-			newPanel.add(WaitTime, constraints);
-
-			constraints.gridx = 0;
-			constraints.gridy = 3;
-			constraints.gridwidth = 7;
-			constraints.gridheight = 1;
-			constraints.anchor = GridBagConstraints.WEST;
-
-			queue = new JTextArea(10, 70);
-			queue.setBounds(320, 75, 260, 260);
-			newPanel.add(queue, constraints);
-			queue.setEditable(false);
 
 			// set border for the panel
 			newPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Philips 115 Lab:"));
@@ -152,10 +110,10 @@ public class Hub extends JFrame{
 
 			pack();
 			setLocationRelativeTo(null);
-			if (!connect) {
+			/*if (!connect) {
 				queue.setText("Unsuccessful Connection to Server");
 				disconnect();
-			}
+			}*/
 
 		}
 	}
